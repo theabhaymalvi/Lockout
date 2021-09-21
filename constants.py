2a9c5f37a5aa6e03939cc71d5e124947ab2842cd
Abhay Malvi <75466244+theabhaymalvi@users.noreply.github.com>
2021-09-21 08:23:45 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
 AUTO_UPDATE_TIME = 20
 
 SERVER_INVITE = "nothing here"
-BOT_INVITE = "https://discord.com/api/oauth2/authorize?client_id=796963446080012299&permissions=0&scope=bot"
-GITHUB_LINK = "https://github.com/DreadArceus/Lockout-Bot"
+BOT_INVITE = "https://discord.com/api/oauth2/authorize?client_id=869208260392136715&permissions=0&scope=bot"
+GITHUB_LINK = "https://github.com/theabhaymalvi/Lockout"
 
 ADMIN_PRIVILEGE_ROLES = ['Admin', 'Moderator', 'Lockout Manager']
-OWNERS = [202776600645861376]
+OWNERS = [783529827130277898]
 
 BACKUP_DIR = "./data/backup/"
 
